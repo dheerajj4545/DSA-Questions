41bd3d4c452c62dd598f6b7a68019fee48a1d8d9
Dheeraj Singh Rao <dheerajsinghrao6@gmail.com>
2026-07-24 10:46:22 +0530

    Time: 4 ms (34.16%), Space: 12.3 MB (29.82%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* dfs(Node* node) {
+        if (node == NULL)
+            return NULL;
+
+        if (mp.find(node) != mp.end())
+            return mp[node];
+
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+
+        for (auto neighbor : node->neighbors) {
+            clone->neighbors.push_back(dfs(neighbor));
+        }
+
+        return clone;
+    }
+
+    Node* cloneGraph(Node* node) {
+        return dfs(node);
+    }
+};
