3d836fd40e2e20adef2466254f0cd7641a30e4cf
Dheeraj Singh Rao <dheerajsinghrao6@gmail.com>
2026-08-02 08:44:00 +0530

    Time: 23 ms (6.61%), Space: 19.8 MB (7.37%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dfs(int i,int j,vector<int>& piles,vector<vector<int>>&dp){
+        if(i==j) return piles[i];
+        if(dp[i][j]!=INT_MIN) return dp[i][j];
+        int forward=piles[i]-dfs(i+1,j,piles,dp);
+        int backward=piles[j]-dfs(i,j-1,piles,dp);
+        return dp[i][j]=max(forward,backward);
+    }
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
+        return dfs(0,n-1,piles,dp);
+        
+    }
+};
