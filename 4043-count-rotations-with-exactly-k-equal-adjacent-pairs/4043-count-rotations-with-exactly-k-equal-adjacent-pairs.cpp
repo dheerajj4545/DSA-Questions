e46efafa9755c0a4975a58809158b7470c2a730c
Dheeraj Singh Rao <dheerajsinghrao6@gmail.com>
2026-09-06 21:46:19 +0530

    Time: 0 ms (100%), Space: 9.5 MB (76.36%) - LeetHub

---
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int n = s.size();
+
+        int equal = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == s[(i + 1) % n]) {
+                equal++;
+            }
+        }
+
+        if (k == equal) {
+            return n - equal;
+        }
+
+        if (k == equal - 1) {
+            return equal;
+        }
+
+        return 0;
+    }
+};
