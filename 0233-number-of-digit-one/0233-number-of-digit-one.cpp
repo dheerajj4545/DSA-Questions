01aa6d4e32e92cbcb98e50164456ae9d28d6d7dd
Dheeraj Singh Rao <dheerajsinghrao6@gmail.com>
2026-09-19 18:43:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (23.18%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countDigitOne(int n) {
+        long long count = 0;
+
+        for (long long i = 1; i <= n; i *= 10) {
+            long long higher = n / (i * 10);
+            long long curr = (n / i) % 10;
+            long long lower = n % i;
+
+            if (curr == 0) {
+                count += higher * i;
+            }
+            else if (curr == 1) {
+                count += higher * i + lower + 1;
+            }
+            else {
+                count += (higher + 1) * i;
+            }
+        }
+
+        return count;
+    }
+};
